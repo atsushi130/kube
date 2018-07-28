b93f4653daab74f68debdee63128a5ae6aca2f11
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 17:39:28 +0900

    add: script for config command

--- Sources/kube/ScriptDefiles.swift
+++ Sources/kube/ScriptDefiles.swift
@@ -10,14 +10,21 @@ import Foundation
 struct ScriptDefines {
     
     // MARK: - kubectl
-    static let kubectlGetPods    = "kubectl get pods"
-    static let kubectlBashLogin  = "xargs -o -Ipods kubectl exec -it pods bash"
-    static let kubectlLogs       = "xargs -Ipods kubectl logs pods api -f"
+    static let kubectlGetPods   = "kubectl get pods"
+    static let kubectlBashLogin = "xargs -o -Ipods kubectl exec -it pods bash"
+    static let kubectlLogs      = "xargs -Ipods kubectl logs pods api -f"
     
-    // MARK: - fzf-tmu
-    static let fzfTmux           = "fzf-tmux --ansi --reverse --prompt"
-    static let fzfTmuxForLogin   = ScriptDefines.fzfTmux + "='login pods is '"
-    static let fzfTmuxForLog     = ScriptDefines.fzfTmux + "='monitoring pods is '"
+    // MARK: GCloud
+    static let gcloudProjectsList     = "gcloud projects list"
+    static let gcloudConfigSetProject = "xargs -Iproject_name gcloud config set project project_name"
+    static let gcloudGetCredentials   = "gcloud container clusters get-credentials api-cluster --zone asia-northeast1-a"
+    static let gcloudChangeProject    = ScriptDefines.gcloudConfigSetProject + "&&" + ScriptDefines.gcloudGetCredentials
+    
+    // MARK: - fzf-tmux
+    static let fzfTmux          = "fzf-tmux --ansi --reverse --prompt"
+    static let fzfTmuxForLogin  = ScriptDefines.fzfTmux + "='login pods is '"
+    static let fzfTmuxForLog    = ScriptDefines.fzfTmux + "='monitoring pods is '"
+    static let fzfTmuxForConfig = ScriptDefines.fzfTmux + "='gcloud config set project '"
     
     // MARK: - System
     static let awk               = "awk 'NR > 1 {print $1}'"
