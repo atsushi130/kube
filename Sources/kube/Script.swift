c634d3274382e01554b5aff42c89509d793d7ea7
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 16:28:25 +0900

    add: Script

---
+++ Sources/kube/Script.swift
@@ -0,0 +1,17 @@
+//
+//  Script.swift
+//  kube
+//
+//  Created by Atsushi Miyake on 2018/07/28.
+//
+
+import Foundation
+
+struct Script {
+    
+    private let script: String
+    
+    init(_ script: String) {
+        self.script = script
+    }
+}
