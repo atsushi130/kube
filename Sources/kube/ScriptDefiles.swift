5f5d7e59b3e23bd9b38a2afd39917a63dbb4949e
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 17:18:22 +0900

    implement: Scripts

--- Sources/kube/ScriptDefiles.swift
+++ Sources/kube/ScriptDefiles.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct ScriptDefines {
+    
+    // MARK: - kubectl
+    static let kubectlGetPods      = "kubectl get pods"
+    static let kubectlExecForlogin = "xargs -o -Ipods kubectl exec -it pods bash"
+    
+    // MARK: - fzf-tmu
+    static let fzfTmux             = "fzf-tmux --ansi --reverse --prompt"
+    static let fzfTmuxForLogin     = ScriptDefines.fzfTmux + "='monitoring pods is '"
+    
+    // MARK: - System
+    static let awk                 = "awk 'NR > 1 {print $1}'"
+    static let grepApiDeployment   = "grep api-deployment"
+}
