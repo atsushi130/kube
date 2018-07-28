86429cb9d5a728043c1271357e9153ce6b3a7203
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 17:29:25 +0900

    implement: log command routing

--- Sources/kube/Kube.swift
+++ Sources/kube/Kube.swift
@@ -4,12 +4,14 @@ import Commandy
 enum Kube: String, Commandy.Cli {
     
     case login
+    case log
     
-    static var allCases: [Kube] = [.login]
+    static var allCases: [Kube] = [.login, .log]
     
     func run() throws {
         switch self {
         case .login: try Login.run()
+        case .log:   try Log.run()
         }
     }
 }
