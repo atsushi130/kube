59e0c451a7c566d780c49694e845550cefff7a8c
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 17:40:00 +0900

    implement: config command routing

--- Sources/kube/Kube.swift
+++ Sources/kube/Kube.swift
@@ -5,13 +5,15 @@ enum Kube: String, Commandy.Cli {
     
     case login
     case log
+    case config
     
-    static var allCases: [Kube] = [.login, .log]
+    static var allCases: [Kube] = [.login, .log, .config]
     
     func run() throws {
         switch self {
-        case .login: try Login.run()
-        case .log:   try Log.run()
+        case .login:  try Login.run()
+        case .log:    try Log.run()
+        case .config: try Config.run()
         }
     }
 }
