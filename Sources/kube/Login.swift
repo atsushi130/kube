19dea117829b566f37aba9b377f792f891fc9c24
atsushi130 <atsushi_main@outlook.jp>
2018-07-29 01:04:15 +0900

    fix: login script

--- Sources/kube/Login.swift
+++ Sources/kube/Login.swift
@@ -10,11 +10,12 @@ import Commandy
 
 struct Login: Commandy.Command {
     static func run() throws {
-        let script = Script.kubectlGetPods
-            | ScriptDefines.awk
-            | ScriptDefines.grepApiDeployment
-            | ScriptDefines.fzfTmuxForLogin
-            | ScriptDefines.kubectlBashLogin
+        let script = Script.builder
+            | "kubectl get pods"
+            | "awk 'NR > 1 {print $1}'"
+            | "grep api-deployment"
+            | "fzf-tmux --ansi --reverse --prompt='login pods is '"
+            | "xargs -o -Ipods kubectl exec -it pods bash"
         script.exec()
     }
 }
