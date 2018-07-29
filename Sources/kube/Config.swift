569a0e690a75f10b5394af10a31e05ea9275f222
atsushi130 <atsushi_main@outlook.jp>
2018-07-30 00:54:17 +0900

    update: fzf script of config command

--- Sources/kube/Config.swift
+++ Sources/kube/Config.swift
@@ -14,7 +14,7 @@ struct Config: Commandy.Command {
             | "gcloud projects list"
             | "awk 'NR > 1 {print $1}'"
             | "grep api-deployment"
-            | "fzf-tmux --ansi --reverse --prompt='gcloud config set project '"
+            | "fzf-tmux -d 35% --preview 'echo ❯ gcloud config set project {}' --preview-window down:1 --ansi --reverse --prompt='set project is '"
             | "xargs -Iname gcloud config set project name && gcloud container clusters get-credentials api-cluster --zone asia-northeast1-a"
         script.exec()
     }
