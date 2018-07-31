5b0831eeea921060af8db09755d05912129ae10e
atsushi130 <atsushi_main@outlook.jp>
2018-07-31 10:37:45 +0900

    implement: Process extension

--- Sources/kube/Process+.swift
+++ Sources/kube/Process+.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+extension Process {
+    
+    static func make(script: String) -> Process {
+        let process = Process()
+        process.launchPath = "/bin/sh"
+        process.arguments = ["-c", script]
+        return process
+    }
+    
+    func launchWithWaitUntilExit() {
+        self.launch()
+        self.waitUntilExit()
+    }
+}
