dda40257f98673ebe26a952f21b4f682720e4fa6
atsushi130 <atsushi_main@outlook.jp>
2018-08-05 01:28:12 +0900

    implement: pods command routing

--- Sources/kube/Kube.swift
+++ Sources/kube/Kube.swift
@@ -6,12 +6,14 @@ enum Kube: String, Commandy.Cli {
     case login
     case log
     case config
+    case pods
     
     func run() throws {
         switch self {
         case .login:  try Login.run()
         case .log:    try Log.run()
         case .config: try Config.run()
+        case .pods:   try Pods.run()
         }
     }
 }
