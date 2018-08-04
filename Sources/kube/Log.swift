28d67207964ed299576918b46e991b43ce9f1d92
atsushi130 <atsushi_main@outlook.jp>
2018-08-05 01:20:25 +0900

    update: prompt string

--- Sources/kube/Log.swift
+++ Sources/kube/Log.swift
@@ -14,7 +14,7 @@ struct Log: Commandy.Command {
             | "kubectl get pods"
             | "awk 'NR > 1 {print $1}'"
             | "grep api-deployment"
-            | "fzf-tmux -d 35% --preview 'echo ❯ kubectl logs {} api -f' --preview-window down:1 --ansi --reverse --prompt='monitoring pods is '"
+            | "fzf-tmux -d 35% --preview 'echo ❯ kubectl logs {} api -f' --preview-window down:1 --ansi --reverse --prompt='monitoring target: '"
             | "xargs -o -Ipods kubectl logs pods api -f"
             | "grep -v -e \"Status\" -e \"/status\" -e \"Completed 200 OK\""
         script.exec()
